fba19a78f37e46b688d5bbea9aa529509d27b349
Colin Mosher <moldy912@gmail.com>
2018-04-04 00:35:05 -0400

    simplify heat map, fix some issues

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -15,6 +15,7 @@ export class TicTacToeComponent implements OnInit {
   state = 'moves';
   starter = false;
   heatmap = false;
+  gameOverMessage = '';
 
   @Output() notify: EventEmitter<any> = new EventEmitter<any>();
   @Input() sessionComp: SessionComponent;
@@ -27,13 +28,11 @@ export class TicTacToeComponent implements OnInit {
   newGame() {
     this.board = new TicTacToeBoard();
     this.switchStarter();
-    if (this.heatmap) { this.getHeatmap(); }
   }
 
   undo() {
     const move = this.board.undo();
     this.sessionComp.session.undoStats();
-    if (this.heatmap) { this.getHeatmap(); }
   }
 
   setState(state) {
@@ -42,10 +41,14 @@ export class TicTacToeComponent implements OnInit {
 
   takeTurn(x, y) {
     const move = new Move(x, y, this.board.whoseTurn);
-    // this.board.takeTurn(move);
     if (this.board.takeTurn(move)) {
       this.notify.emit({board: this.board, move: move});
-      if (this.heatmap) { this.getHeatmap(); }
+      if (this.board.winner) {
+        this.gameOverMessage = `Congrats to Player ${this.board.winningPlayer} on winning!`;
+      }
+      if (this.board.moveLog.length === 9 && !this.board.winner) {
+        this.gameOverMessage = `No one won! Would you like to play again?`;
+      }
     }
   }
 
@@ -55,40 +58,11 @@ export class TicTacToeComponent implements OnInit {
     }
   }
 
-  toggleHeatmap() {
-    if (this.heatmap) { this.getHeatmap(); }
-    else { $('td.game-cell').css('background-color', 'white'); }
-    // this.getHeatmap();
-  }
-
-  getHeatmap() {
-    const sess = this.sessionComp.session;
-    const min = sess.minFreq;
-    const max = sess.maxFreq;
-    const self = this;
-    console.log("getting heat map");
-    if (this.heatmap) {
-      $('td.game-cell').each(function(index) {
-        const x = Math.floor(index / 3);
-        const y = Math.floor(index % 3);
-        const heat = sess.moveFreq[x][y];
-        const color = self.getHeatColor(heat, min, max);
-        //console.log(`${x},${y} : ${color}`);
-        $(this).css('background-color', color);
-        //console.log($(this).css("background-color"));
-      });
-    }
-  }
-
   getHeatColor(value, min, max) {
     const cMax = '0000FF';
     const white = 'FFFFFF';
     const cMin = 'FF0000';
     const ratio = (value - min) / (max - min);
-    // const hex = function(x) {
-    //   x = x.toString(16);
-    //   return (x.length == 1) ? '0' + x : x;
-    // };
 
     //if (ratio > 0.5) {
       const r = Math.ceil(parseInt(cMin.substring(0, 2), 16) * ratio + parseInt(white.substring(0, 2), 16) * (1 - ratio));
@@ -99,8 +73,16 @@ export class TicTacToeComponent implements OnInit {
       // const g = Math.ceil(parseInt(white.substring(2, 4), 16) * ratio + parseInt(cMax.substring(2, 4), 16) * (1 - ratio));
       // const b = Math.ceil(parseInt(white.substring(4, 6), 16) * ratio + parseInt(cMax.substring(4, 6), 16) * (1 - ratio));
     //}
-
     return `rgba(${r},${g},${b}, 0.75)`;
+  }
 
+  getHeat(x, y) {
+    const sess = this.sessionComp.session;
+    const min = sess.minFreq;
+    const max = sess.maxFreq;
+    const value = sess.moveFreq[x][y];
+    if (this.heatmap) {
+      return {'background-color': this.getHeatColor(value, min, max) };
+    }
   }
 }
